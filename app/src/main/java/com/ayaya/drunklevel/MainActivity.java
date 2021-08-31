6b2bba73deaf38eba7e8d970803db9a47074b113
PTLC_ <ptlc.ptlc.ptlc@gmail.com>
2021-08-31 22:31:23 +0200

    Ajout de requetes vers les autres activités

--- app/src/main/java/com/ayaya/drunklevel/MainActivity.java
+++ app/src/main/java/com/ayaya/drunklevel/MainActivity.java
@@ -1,15 +1,70 @@
 package com.ayaya.drunklevel;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    
+    SharedPreferences save;
+    List<Conso> consos;
+    
+    ActivityResultLauncher<Object> getConso;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String test;
+        
+        // récupération de la sauvegarde et de l'historique des consommations
+        save = getSharedPreferences("save", MODE_PRIVATE);
+        try {
+            JSONArray jsonConsos = new JSONArray(save.getString("consos", "[]"));
+            for (int i = 0; i < jsonConsos.length(); i++)
+                consos.add(Conso.fromJSON(jsonConsos.getJSONObject(i)));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        
+        // création d'un moyen de lancer les activités permettant la demande de ce qui a été consommé
+        getConso = registerForActivityResult(new ActivityResultContract<Object, Conso>() {
+            @Override
+            public Intent createIntent(Context context, Object input) {
+                return new Intent(context, SelectDrinkActivity.class);
+            }
+            @Override
+            public Conso parseResult(int resultCode, Intent intent) {
+                if (resultCode == RESULT_OK && intent.getFloatExtra("volume", -1f) != -1f && intent.getStringExtra("drink") != null)
+                    return new Conso(new Date().getTime(), Drink.fromJSON(intent.getStringExtra("drink")), intent.getFloatExtra("volume", -1f));
+                return null;
+            }
+        }, new ActivityResultCallback<Conso>() {
+            @Override
+            public void onActivityResult(Conso conso) {
+                if (conso == null)
+                    return;
+                consos.add(conso);
+                Toast.makeText(getApplicationContext(), conso.getTime()+" "+conso.getVolume()+"L de "+conso.getDrink().getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+        
+        // Si le sexe ou le poids n'est pas dans la sauvegarde, on les demande
+        if (save.getInt("sex", -1) == -1 || save.getFloat("weight", -1f) == -1f) {
+            Intent sexIntent = new Intent(getApplicationContext(), QuerySex.class);
+            startActivity(sexIntent);
+        }
     }
 }
